572106f43d380556bb68f49329f76ac49850c6f3
Nathan Andrew Winkel <55713536+NathanW061@users.noreply.github.com>
2026-04-04 20:02:41 +0000

    Implemented basic chatbot functionality and added L'Oreal logo to page.

--- script.js
+++ script.js
@@ -3,16 +3,47 @@ const chatForm = document.getElementById("chatForm");
 const userInput = document.getElementById("userInput");
 const chatWindow = document.getElementById("chatWindow");
 
+let messageHistory = [
+  {
+    role: 'system',
+    content: 'You are a chatbot that answers questions about, and gives advice/recommendations related to, L\'Oréal brand products. If the user asks a question that would be relevant to one or more L\'Oréal product(s) or the products L\'Oréal carries in general, such as beauty-related topics, go ahead and answer them. Otherwise, politely decline to answer the question and offer suggestions on topics you can respond to.'
+  }
+];
+
 // Set initial message
 chatWindow.textContent = "👋 Hello! How can I help you today?";
 
 /* Handle form submit */
-chatForm.addEventListener("submit", (e) => {
+chatForm.addEventListener("submit", async (e) => {
   e.preventDefault();
+
+  messageHistory.push({
+    role: 'user',
+    content: userInput.value
+  });
 
   // When using Cloudflare, you'll need to POST a `messages` array in the body,
   // and handle the response using: data.choices[0].message.content
+  chatWindow.textContent = "Waiting for a response...";
+  let response = await fetch('https://loreal-chatbot-security.nathan-winkel.workers.dev/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      messages: messageHistory
+    })
+  });
+
+  if(!response.ok)
+  {
+    chatWindow.textContent = "Oops! Something went wrong, please try again.";
+    return;
+  }
 
   // Show message
-  chatWindow.innerHTML = "Connect to the OpenAI API for a response!";
+  const data = await response.json();
+  console.log(data);
+
+  chatWindow.innerHTML = data.choices[0].message.content;
 });
